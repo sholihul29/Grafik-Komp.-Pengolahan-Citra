0d2cbb89278d8417ee2ad838b92ee9e7fe69c04b
sholihul29 <55776593+sholihul29@users.noreply.github.com>
2019-09-30 00:00:19 +0700

    Add files via upload

---
+++ tes.c
@@ -0,0 +1,55 @@
+main()
+{int x, y ,a;
+awal:
+for(x=0;x<67; x++)
+{space(x);
+printf("Erwin Firdaus");
+for(a=0;a<5000;a++)
+for(y=0;y<1000;y++);
+system("cls");
+}
+
+for(x=61;x>0; x--)
+{space(x);
+printf("Shara Mustika Putri");
+for(a=0;a<5000;a++)
+for(y=0;y<1000;y++);
+system("cls");
+}
+for(x=0;x<67; x++)
+{space(x);
+printf("Solihul Hadi");
+for(a=0;a<5000;a++)
+for(y=0;y<1000;y++);
+system("cls");
+}
+
+for(x=67;x>0; x--)
+{space(x);
+printf("Erwin Firdaus");
+for(a=0;a<5000;a++)
+for(y=0;y<1000;y++);
+system("cls");
+}
+for(x=0;x<61; x++)
+{space(x);
+printf("Shara Mustika Putri");
+for(a=0;a<5000;a++)
+for(y=0;y<1000;y++);
+system("cls");
+}
+
+for(x=67;x>0; x--)
+{space(x);
+printf("Sholihul Hadi");
+for(a=0;a<5000;a++)
+for(y=0;y<1000;y++);
+system("cls");
+}
+goto awal;
+}
+space(int x)
+{int a;
+for(a=0;a<x;a++)
+printf(" ");
+}
